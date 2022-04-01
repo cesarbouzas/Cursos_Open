536cd5b3451547df0eb6d179e684d63ef5dcb773
cesar <cesarbouzaspaw@gmail.com>
2022-04-01 18:47:46 +0200

    php

---
+++ 2204_PHP/220401_Arrays.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practicas Arrays</title>
+</head>
+<body>
+    <?php
+    $dias=array("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
+    $meses=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","septiembre","Octubre","Noviembre","Diembre"];
+    $vacio=array();
+    $vacio2=[];
+    $sinDefinir=array(15);
+
+    var_dump($dias);
+    var_dump($meses);
+    var_dump($vacio);
+    var_dump($vacio2);
+    var_dump($sinDefinir);
+    ?>    
+</body>
+</html>
